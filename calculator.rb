18e47cec8e03bffee5b900bea9fa0eff837a39c3
sarah <s.j.condon01@gmail.com>
2019-09-09 05:00:59 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -6,4 +6,9 @@ describe "./calculator.rb" do
 
     expect(first_number).to be_an(Integer).or be_a(Float)
   end
-  
+   it "contains a local variable called second_number that is assigned to a number that isn't 0" do
+    second_number = get_variable_from_file('./calculator.rb', "second_number")
+
+    expect(second_number).to be_an(Integer).or be_a(Float)
+    expect(second_number).not_to equal(0)
+  end
